e6307a83f059e7d426ca4bdc46b1af845b5f195f
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-05 16:40:15 +0200

    task 0.4 is complete

--- src/com/umuzi/Task.java
+++ src/com/umuzi/Task.java
@@ -9,4 +9,12 @@ public class Task {
     public void hello(String name){
         System.out.println("Hello " + name);
     }
+
+    public void evenOrOdd(int num){
+        double remainder = num%2;
+        if (remainder != 0)
+            System.out.println("Odd");
+        else
+            System.out.println("Even");
+    }
 }
