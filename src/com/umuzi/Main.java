c96763e10ccfa375989609b198ddf34b5a110e83
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-05 15:31:13 +0200

    deleted unncessary git file

--- src/com/umuzi/Main.java
+++ src/com/umuzi/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         taskOne();
+        taskTwo();
 
     }
 
     private static void taskOne() {
         int x = 0;
         int y = 1;
+        System.out.println("Task 0.1");
         System.out.println(x);
         System.out.println(y);
         x = x + 3;
@@ -19,8 +21,17 @@ public class Main {
         System.out.println(y);
     }
 
-    private void taskTwo() {
+    private static void taskTwo() {
+        int x,y,z,a,b;
 
+        x = 1 + 1 * 2;
+        y = (1 + 1) * 2;
+        z = 1 + (1 * 2);
+        a = 1 + 1 * 2 / 2;
+        b = (1 + 1 * 2) / 2;
+
+        System.out.println("Task 0.2");
+        System.out.println(x + "\n" + y + "\n" + z + "\n" + a + "\n" + b);
     }
 
     private void taskThree() {
