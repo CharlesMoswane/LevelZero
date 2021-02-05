74ef8e17576d176c888d965e892dbe6bc84a2a06
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-05 14:45:57 +0200

    first commit

---
+++ src/com/umuzi/Main.java
@@ -0,0 +1,49 @@
+package com.umuzi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+    private void taskOne() {
+
+    }
+
+    private void taskTwo() {
+
+    }
+
+    private void taskThree() {
+
+    }
+
+    private void taskFour() {
+
+    }
+
+    private void taskFive() {
+
+    }
+
+    private void taskSix() {
+
+    }
+
+    private void taskSeven() {
+
+    }
+
+    private void taskEight() {
+
+    }
+
+    private void taskNine() {
+
+    }
+
+    private void taskTen() {
+
+    }
+}
