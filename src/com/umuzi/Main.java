630845addd9d6dbe91c4e17196265832d398d31b
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-05 15:13:02 +0200

    task 0.1 complete

--- src/com/umuzi/Main.java
+++ src/com/umuzi/Main.java
@@ -4,11 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        taskOne();
 
     }
 
-    private void taskOne() {
-
+    private static void taskOne() {
+        int x = 0;
+        int y = 1;
+        System.out.println(x);
+        System.out.println(y);
+        x = x + 3;
+        y = y + x;
+        System.out.println(x);
+        System.out.println(y);
     }
 
     private void taskTwo() {
