d61d776c784df97792e8f1401b5f2417edeb3443
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-07 15:46:27 +0200

    task 0.8 complete

--- src/com/umuzi/Task.java
+++ src/com/umuzi/Task.java
@@ -59,4 +59,21 @@ public class Task {
         return temperatureInFahrenheit;
     }
 
+    //Task 0.8
+    public void hoursAndMinutes(int number){
+
+        int hours = number/60;
+        int minutes = number%60;
+
+        if(hours > 1 && minutes > 1)
+            System.out.println(hours + " hours, " + minutes + " minutes");
+        else if(hours > 1 && minutes <= 1)
+            System.out.println(hours + " hours, " + minutes + " minute");
+        else if(hours <= 1 && minutes > 1)
+            System.out.println(hours + " hour, " + minutes + " minutes");
+        else
+            System.out.println(hours + " hour, " + minutes + " minute");
+
+    }
+
 }
