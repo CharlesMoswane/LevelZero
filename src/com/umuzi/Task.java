7e1c11ce40af8333139cfd5e95544f51bf846462
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-08 14:30:41 +0200

    task 0.10 complete

--- src/com/umuzi/Task.java
+++ src/com/umuzi/Task.java
@@ -91,4 +91,21 @@ public class Task {
         }
 
     }
+
+    //Task 0.10
+    public void commonLetters(String word1, String word2){
+
+        System.out.println(word1);
+        System.out.println(word2);
+
+        String lowerCaseWord1 = word1.toLowerCase();
+        String lowerCaseWord2 = word2.toLowerCase();
+        char[] word2Letters = lowerCaseWord2.toCharArray();
+
+        for (char l : word2Letters){
+            String letter = Character.toString(l);
+            if (lowerCaseWord1.contains(letter))
+                System.out.print(letter + " ");
+        }
+    }
 }
