ce665876b9433bd68a5ef7b33320530242828c3c
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-08 14:03:09 +0200

    task 0.9 complete

--- src/com/umuzi/Task.java
+++ src/com/umuzi/Task.java
@@ -76,4 +76,19 @@ public class Task {
 
     }
 
+    //Task 0.9
+    public void printVowels(String word){
+
+        System.out.println(word);
+
+        char[] vowels = {'a','e','i','o','u'};
+        String lowerCaseWord = word.toLowerCase();
+
+        for (char v : vowels){
+            String vowel = Character.toString(v);
+            if (lowerCaseWord.contains(vowel))
+                System.out.print(vowel + " ");
+        }
+
+    }
 }
