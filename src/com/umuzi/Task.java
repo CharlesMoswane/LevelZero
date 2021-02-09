30c48de57f2dd7934267e4c1ddb532d71c15af8c
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-09 17:55:32 +0200

    moved task 0.4

--- src/com/umuzi/Task.java
+++ src/com/umuzi/Task.java
@@ -6,16 +6,6 @@ public class Task {
 
     }
 
-    //Task 0.4
-    public void evenOrOdd(int num){
-        System.out.println("\n" + "Task 0.4");
-
-        double remainder = num%2;
-        if (remainder != 0)
-            System.out.println("Odd");
-        else
-            System.out.println("Even");
-    }
 
     //Task 0.5
     public double areaOfATriangle(double sideA,double sideB,double sideC){
---
+++ src/com/umuzi/TaskFour.java
@@ -0,0 +1,14 @@
+package com.umuzi;
+
+public class TaskFour {
+    //Task 0.4
+    public void evenOrOdd(int num){
+        System.out.println("\n" + "Task 0.4");
+
+        double remainder = num%2;
+        if (remainder != 0)
+            System.out.println("Odd");
+        else
+            System.out.println("Even");
+    }
+}
