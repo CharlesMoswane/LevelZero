adb39a7e669bb1906c56df69d56dc4edcccffc71
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-09 18:22:32 +0200

    moved task 0.10

---
+++ src/com/umuzi/TaskTen.java
@@ -0,0 +1,20 @@
+package com.umuzi;
+
+public class TaskTen {
+    //Task 0.10
+    public void commonLetters(String word1, String word2){
+        System.out.println("\n" + "Task 0.10" + "\n");
+        System.out.println(word1);
+        System.out.println(word2);
+
+        String lowerCaseWord1 = word1.toLowerCase();
+        String lowerCaseWord2 = word2.toLowerCase();
+        char[] word2Letters = lowerCaseWord2.toCharArray();
+
+        for (char l : word2Letters){
+            String letter = Character.toString(l);
+            if (lowerCaseWord1.contains(letter))
+                System.out.print(letter + " ");
+        }
+    }
+}
