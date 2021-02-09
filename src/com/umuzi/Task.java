bad6e4fb97c67c71b932b26290cf58d975cc8ac8
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-09 18:04:48 +0200

    moved task 0.8

--- src/com/umuzi/Task.java
+++ src/com/umuzi/Task.java
@@ -6,23 +6,6 @@ public class Task {
 
     }
 
-    //Task 0.8
-    public void hoursAndMinutes(int number){
-        System.out.println("\n" + "Task 0.9");
-
-        int hours = number/60;
-        int minutes = number%60;
-
-        if(hours > 1 && minutes > 1)
-            System.out.println(hours + " hours, " + minutes + " minutes");
-        else if(hours > 1 && minutes <= 1)
-            System.out.println(hours + " hours, " + minutes + " minute");
-        else if(hours <= 1 && minutes > 1)
-            System.out.println(hours + " hour, " + minutes + " minutes");
-        else
-            System.out.println(hours + " hour, " + minutes + " minute");
-    }
-
     //Task 0.9
     public void printVowels(String word){
         System.out.println("\n" + "Task 0.9" + "\n");
