1c21384a73fae09009016611ff10db2c4577ed15
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-13 10:51:53 +0200

    fixed task 0.3 and 0.8

--- src/com/umuzi/Main.java
+++ src/com/umuzi/Main.java
@@ -6,11 +6,13 @@ public class Main {
         TaskOne task1 = new TaskOne();
         TaskTwo task2 = new TaskTwo();
         TaskThree task3 = new TaskThree();
+        TaskEight task8 = new TaskEight();
 
         //Example of function calls
         task1.taskOne();
         task2.taskTwo();
         task3.hello("Charles");
+        task8.hoursAndMinutes(180);
     }
 
 }
--- src/com/umuzi/TaskThree.java
+++ src/com/umuzi/TaskThree.java
@@ -4,7 +4,7 @@ public class TaskThree {
 
     public void hello(String name){
         System.out.println("\n" + "Task 0.3");
-        System.out.println("Hello " + name);
+        System.out.println("Hello " + name + "!");
     }
 
 }
