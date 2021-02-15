3e77c45e38d4df9a056b7d1ced345f34c272e4e3
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-02-15 14:47:29 +0200

    added a function to remove duplicate characters in Task 1.10

--- src/com/umuzi/TaskTen.java
+++ src/com/umuzi/TaskTen.java
@@ -10,13 +10,28 @@ public class TaskTen {
         char[] word2Letters = lowerCaseWord2.toCharArray();
         String commonLetters = "";
 
+        System.out.println("Common Letters:");
         for (char l : word2Letters){
             String letter = Character.toString(l);
-            if (lowerCaseWord1.contains(letter) && !(commonLetters.contains(letter)))
-                 commonLetters =  commonLetters + letter + " ";
+            if (lowerCaseWord1.contains(letter)){
+                System.out.print(l + " ");
+                lowerCaseWord1 = removeDuplicateCharacters(lowerCaseWord1,letter);
+            }
         }
 
         System.out.println(commonLetters);
+    }
+
+    public static String removeDuplicateCharacters(String word, String character){
+        StringBuilder sb = new StringBuilder(word);
+        int index = sb.indexOf(character);
+
+        while (index != -1){
+            sb.deleteCharAt(index);
+            index = sb.indexOf(character);
+        }
+
+        return sb.toString();
     }
 
 }
