e117ee63466a6d3a976118d842375ec5b7e2eabc
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-07-06 19:33:10 +0200

    Fixed the temperature conversion errors.

--- src/com/umuzi/Main.java
+++ src/com/umuzi/Main.java
@@ -9,16 +9,19 @@ public class Main {
         TaskFour task4 = new TaskFour();
         TaskFive task5 = new TaskFive();
         TaskSix task6 = new TaskSix();
+        TaskSeven task7 = new TaskSeven();
         TaskEight task8 = new TaskEight();
         TaskNine task9 = new TaskNine();
         TaskTen task10 = new TaskTen();
 
 //        task1.taskOne();
-        task2.taskTwo();
+//        task2.taskTwo();
 //        task3.hello("Charles");
 //        task4.evenOrOdd(3);
-//        System.out.println(task6.maximum(3,7,4));
 //        System.out.println(task5.areaOfATriangle(5,3,4));
+//        System.out.println(task6.maximum(3,7,4));
+//        System.out.println(task7.celsiusToFahrenheit(20));
+        System.out.println(task7.fahrenheitToCelsius(68));
 //        task8.hoursAndMinutes(180);
 //        task8.hoursAndMinutes(18);
 //        task9.printVowels("Heey There");
--- src/com/umuzi/TaskSeven.java
+++ src/com/umuzi/TaskSeven.java
@@ -3,13 +3,13 @@ package com.umuzi;
 public class TaskSeven {
 
     public double celsiusToFahrenheit(double temperature){
-        double temperatureInFahrenheit = (temperature * (9/5)) + 32;
+        double temperatureInFahrenheit = (temperature * (double) 9 / 5) + 32;
 
         return temperatureInFahrenheit;
     }
 
     public double fahrenheitToCelsius(double temperature) {
-        double temperatureInFahrenheit = (temperature - 32) * (5 / 9);
+        double temperatureInFahrenheit = (temperature - 32) * (double)5 / 9;
 
         return temperatureInFahrenheit;
     }
