a2b86b24c6d679263366f68ac8e60259834c7476
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-07-06 19:10:02 +0200

    Cleaned up the main method

--- src/com/umuzi/Main.java
+++ src/com/umuzi/Main.java
@@ -6,44 +6,23 @@ public class Main {
         TaskOne task1 = new TaskOne();
         TaskTwo task2 = new TaskTwo();
         TaskThree task3 = new TaskThree();
-
-        //Example of function calls
-        task1.taskOne();
-        task2.taskTwo();
-        task3.hello("Charles");
-
-        //Fixing errors
-        //Task 8
-        TaskEight task8 = new TaskEight();
-        task8.hoursAndMinutes(180);
-        task8.hoursAndMinutes(18);
-
-        //Task 5
+        TaskFour task4 = new TaskFour();
         TaskFive task5 = new TaskFive();
-        double area = task5.areaOfATriangle(5,3,4);
-        System.out.println(area);
-
-        //Task 6
         TaskSix task6 = new TaskSix();
-        double max = 0;
-        max = task6.maximum(3,7,4);
-        System.out.println(max);
-
-        max = task6.maximum(1,2,3,4,5,6);
-        System.out.println(max);
-
-        //Task 9
+        TaskEight task8 = new TaskEight();
         TaskNine task9 = new TaskNine();
-        task9.printVowels("Heey There");
-        //I cannot replicate the problem. The vowel is only printed once.
-        /*Earlier it was requested that the repeating vowels should not be
-          printed out more than once. I am thus baffled by the request to "Print
-          out all the vowels." Does this override the earlier requirement or
-          it means something entirely different?*/
-
-        //Task 10
         TaskTen task10 = new TaskTen();
-        task10.commonLetters("helloo","wooorld");
+
+//        task1.taskOne();
+        task2.taskTwo();
+//        task3.hello("Charles");
+//        task4.evenOrOdd(3);
+//        System.out.println(task6.maximum(3,7,4));
+//        System.out.println(task5.areaOfATriangle(5,3,4));
+//        task8.hoursAndMinutes(180);
+//        task8.hoursAndMinutes(18);
+//        task9.printVowels("Heey There");
+//        task10.commonLetters("helloo","wooorld");
 
     }
 
